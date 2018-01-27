0419b16c8281c3dae4db06e75b935d5e46b0d88f
isszz <10218431@qq.com>
2018-01-27 15:42:25 +0800

    Update and rename LICENSE.md to Sign.js

---
+++ LICENSE.md/Sign.js
@@ -0,0 +1,60 @@
+import md5 from 'md5';
+
+function time(params: Object) {
+    let time = Date.parse( new Date() ).toString();
+    return time.substr(0, 10);
+}
+
+function sign(params: Object) {
+
+    if(!params.timestamp) {
+        params.timestamp = time();
+    }
+
+    let keys = Object.keys(params).sort();
+    let sorted = {};
+
+    for (let k in keys) {
+        sorted[keys[k]] = params[keys[k]];
+    }
+
+    let string = '';
+    for (let key in sorted) {
+        let v = sorted[key];
+        if(key != 'sign' && v != '' && typeof(v) != Object) {
+            string += key + '=' + v + '&';
+        }
+    }
+
+    string += 'secret=kjhsdfhksjhf239ur2hfehf23'; // Replace with your secret
+    string = md5(string);
+    return string.toUpperCase();
+}
+
+function getUrl(url: string, params: Object, isUnSign: boolean) {
+    let result = url
+    if (result.substr(result.length - 1) != '?') {
+        result = result + `?`
+    }
+
+    // unset sign
+    if(!isUnSign) {
+        params.sign = sign(params);
+    }
+    
+    for (let key in params) {
+        let value = params[key]
+        result += `${encodeURIComponent(key)}=${encodeURIComponent(value)}&`
+    }
+
+    result = result.substring(0, result.length - 1);
+    console.log(result);
+    return result;
+}
+
+export default {
+    md5,
+    time,
+    getUrl,
+    sign,
+}
