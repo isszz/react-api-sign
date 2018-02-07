7cc16d2a1894c57c7181f845a3a65a92eebb706a
isszz <10218431@qq.com>
2018-02-07 18:31:41 +0800

    Create demo.php

---
+++ demo.php
@@ -0,0 +1,25 @@
+<?php
+
+require SignSrv.php;
+
+$sign = empty($_GET['sign']) ? (empty($_POST['sign']) ? '' : $_POST['sign']) : $_GET['sign'];
+$timestamp = empty($_GET['timestamp']) ? ignSrv(empty($_POST['timestamp']) ? '' : $_POST['timestamp']) : $_GET['timestamp'];
+
+if (empty($sign)) {
+    exit(json_encode(['code' => 1003, 'message' => '缺少参数sign']));
+}
+
+if (empty($timestamp)) {
+    exit(json_encode(['code' => 1004, 'message' => '缺少参数timestamp']));
+}
+
+$sign = trim($sign);
+$timestamp = trim($timestamp);
+
+if ((Helper::time() - $timestamp) > 1800) {
+    exit(json_encode(['code' => 1005, 'message' => '请求过期']));
+}
+
+if (!SignSrv::check()) {
+    exit(json_encode(['code' => 1006, 'message' => 'invalid sign']));
+}
